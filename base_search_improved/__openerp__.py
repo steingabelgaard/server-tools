f6ff04cf7643384cd526bab8498a5104b700cb6e
Hans Henrik Gabelgaard <hhg@gabelgaard.org>
2016-06-14 14:07:47 +0200

    Task1056: Base search improved - Inspired from https://github.com/dreispt/server-tools/tree/8.0-search-imp/base_search_improved

--- base_search_improved/__openerp__.py
+++ base_search_improved/__openerp__.py
@@ -2,14 +2,16 @@
 # © 2016 Daniel Reis
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    'name': 'Improved Search',
-    'summary': 'Better search using special tokens',
+    'name': 'Improved Name Search',
+    'summary': 'Friendlier search when typing in relation fields',
     'version': '8.0.1.0.0',
     'category': 'Uncategorized',
     'website': 'https://odoo-community.org/',
     'author': 'Daniel Reis, Odoo Community Association (OCA)',
     'license': 'AGPL-3',
-    'data': ['security/ir.model.access.csv'],
+    'data': [
+        'views/ir_model.xml',
+    ],
     'installable': True,
     'depends': [
         'base',
--- base_search_improved/models/__init__.py
+++ base_search_improved/models/__init__.py
@@ -1,2 +1,2 @@
 # -*- coding: utf-8 -*-
-from . import base_model
+from . import ir_model
