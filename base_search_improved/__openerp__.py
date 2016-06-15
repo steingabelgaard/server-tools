c9cda941a11c307183f41287c546ccbd7b6ddc95
Hans Henrik Gabelgaard <hhg@gabelgaard.org>
2016-06-15 10:04:56 +0200

    Task1056: Base Search Improved
    From
    https://github.com/dreispt/server-tools/tree/8.0-search/base_search_improved

---
+++ base_search_improved/__openerp__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# © 2016 Daniel Reis
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': 'Improved Search',
+    'summary': 'Fuzzy search: use wildcards ("%") in place of spaces',
+    'version': '8.0.1.0.0',
+    'category': 'Uncategorized',
+    'website': 'https://odoo-community.org/',
+    'author': 'Daniel Reis, Odoo Community Association (OCA)',
+    'license': 'AGPL-3',
+    'installable': True,
+    'depends': [
+        'base',
+    ],
+}
