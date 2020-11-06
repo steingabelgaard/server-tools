48f410c34de771e598bb666c9baeada180140aa7
Hans Henrik Gabelgaard <hhg@gabelgaard.org>
2020-11-06 09:27:20 +0100

    [MIG] oauth_provider: Migration to 12.0

--- oauth_provider/__manifest__.py
+++ oauth_provider/__manifest__.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
 # Copyright 2016 SYLEAM
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "OAuth Provider",
     "summary": "Allows to use Odoo as an OAuth2 provider",
-    "version": "10.0.1.0.0",
+    "version": "12.0.1.0.0",
     "category": "Authentication",
     "website": "http://www.syleam.fr/",
     "author": "SYLEAM, Odoo Community Association (OCA)",
